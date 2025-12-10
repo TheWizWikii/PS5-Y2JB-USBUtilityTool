597c7c7e8a6ef0757786cd88a6fba3918d9fe14a
MaNu <37479586+TheWizWikii@users.noreply.github.com>
2025-12-10 12:22:02 +0100

    Add files via upload

---
+++ USBUtilityTool/USBUtilityTool/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace USBUtilityTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbUSBSelector = new System.Windows.Forms.ComboBox();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbUSBSelector
+            // 
+            this.cmbUSBSelector.FormattingEnabled = true;
+            this.cmbUSBSelector.Location = new System.Drawing.Point(222, 34);
+            this.cmbUSBSelector.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbUSBSelector.Name = "cmbUSBSelector";
+            this.cmbUSBSelector.Size = new System.Drawing.Size(254, 44);
+            this.cmbUSBSelector.TabIndex = 0;
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFormat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormat.Location = new System.Drawing.Point(500, 28);
+            this.btnFormat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(345, 77);
+            this.btnFormat.TabIndex = 1;
+            this.btnFormat.Text = "Formatear";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.Location = new System.Drawing.Point(867, 28);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(345, 77);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Descargar";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(217, 136);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(31, 29);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "...";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(1071, 136);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(141, 29);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "TheWizWiki";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1241, 183);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.cmbUSBSelector);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3, 88, 3, 4);
+            this.Text = "PS5 Y2JB USB AUTO DOWNLOADER (4.XX - 10.01)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbUSBSelector;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ USBUtilityTool/USBUtilityTool/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.IO.Compression; // Importante para manejar archivos ZIP
+using System.Management;     // Importante para listar unidades (requiere referencia)
+
+namespace USBUtilityTool
+{
+    public partial class Form1 : Form
+    {
+        // (USA TU ENLACE REAL AQUÍ)
+        private const string FIXED_DOWNLOAD_URL = "https://github.com/TheWizWikii/PS5-Stuff/releases/download/1/PS5.zip";
+        public Form1()
+        {
+            InitializeComponent();
+            // Carga las unidades al iniciar el formulario
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Llamar a la función que llena el ComboBox
+            LoadUSBDrives();
+        }
+
+        // =================================================================
+        // FUNCIÓN DE UTILIDAD: Cargar Unidades USB
+        // =================================================================
+        private void LoadUSBDrives()
+        {
+            cmbUSBSelector.Items.Clear();
+            try
+            {
+                // Usa DriveInfo para listar todas las unidades
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+                foreach (DriveInfo d in allDrives)
+                {
+                    // Filtra solo unidades extraíbles (USB) y que estén listas
+                    if (d.DriveType == DriveType.Removable && d.IsReady)
+                    {
+                        // Añade solo la letra de la unidad (ej. "E:")
+                        cmbUSBSelector.Items.Add(d.Name.Substring(0, 2));
+                    }
+                }
+
+                if (cmbUSBSelector.Items.Count > 0)
+                {
+                    cmbUSBSelector.SelectedIndex = 0;
+                    lblStatus.Text = "Unidades USB detectadas. Listo.";
+                }
+                else
+                {
+                    lblStatus.Text = "No se detectaron unidades extraíbles.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"Error al cargar unidades: {ex.Message}";
+            }
+        }
+
+        // =================================================================
+        // FUNCIÓN 1: FORMATO A EXFAT
+        // (Requiere permisos de Administrador para funcionar)
+        // =================================================================
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            string selectedDrive = cmbUSBSelector.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectedDrive))
+            {
+                MessageBox.Show("Por favor, seleccione una unidad USB válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show($"ADVERTENCIA: ¿Desea formatear {selectedDrive} a exFAT? ¡SE PERDERÁN TODOS LOS DATOS!", "Confirmar Formato", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                lblStatus.Text = $"Iniciando formato de {selectedDrive}...";
+
+                // Usamos Task.Run para no bloquear la interfaz de usuario (UI)
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        // Comando: format X: /FS:exFAT /Q /Y
+                        string arguments = $"{selectedDrive} /FS:exFAT /Q /Y";
+
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = "format.com",
+                            Arguments = arguments,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true
+                        };
+
+                        using (Process process = Process.Start(startInfo))
+                        {
+                            process.WaitForExit();
+
+                            // Usamos Invoke para actualizar la UI desde el hilo de fondo
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                if (process.ExitCode == 0)
+                                {
+                                    lblStatus.Text = $"✅ Formato de {selectedDrive} completado exitosamente.";
+                                }
+                                else
+                                {
+                                    string errorOutput = process.StandardError.ReadToEnd() + process.StandardOutput.ReadToEnd();
+                                    lblStatus.Text = $"❌ Error al formatear (Código: {process.ExitCode}). ¿Ejecutó como Administrador? \n Detalle: {errorOutput.Trim()}";
+                                }
+                            });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            lblStatus.Text = $"Error inesperado al formatear: {ex.Message}";
+                        });
+                    }
+                });
+            }
+        }
+
+        // =================================================================
+        // FUNCIÓN 2: DESCARGA Y DESCOMPRESIÓN ZIP
+        // =================================================================
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            string selectedDrive = cmbUSBSelector.SelectedItem?.ToString();
+
+            // FIJAMOS LA URL A LA CONSTANTE EN LUGAR DE LEER EL TEXTBOX
+            string url = FIXED_DOWNLOAD_URL;
+
+            if (string.IsNullOrEmpty(selectedDrive)) // Solo necesitamos verificar la unidad
+            {
+                MessageBox.Show("Por favor, seleccione una unidad USB válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Si la URL fija no tiene el formato correcto, esto actuará como una verificación:
+            if (string.IsNullOrEmpty(url) || !Uri.TryCreate(url, UriKind.Absolute, out _))
+            {
+                MessageBox.Show("Error de configuración: La URL de descarga no es válida.", "Error Crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // --- 1. Preparar rutas ---
+            string tempDirectory = Path.GetTempPath();
+            string fileName;
+            try
+            {
+                fileName = Path.GetFileName(new Uri(url).LocalPath);
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("La URL ingresada no es válida.", "Error de URL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = "downloaded_archive.zip";
+            }
+
+            string tempZipPath = Path.Combine(tempDirectory, fileName);
+            string destinationPath = selectedDrive;
+
+            // Asegurar la limpieza en caso de fallos anteriores
+            if (File.Exists(tempZipPath))
+            {
+                try { File.Delete(tempZipPath); } catch { /* Ignorar */ }
+            }
+
+            try
+            {
+                // --- 2. Descargar el Archivo (Asíncrono) ---
+                lblStatus.Text = $"Iniciando descarga de {fileName} a la carpeta temporal...";
+
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadProgressChanged += (s, args) =>
+                    {
+                        // Actualiza el estado con el porcentaje de descarga
+                        this.Invoke((MethodInvoker)delegate {
+                            lblStatus.Text = $"Descargando... {args.ProgressPercentage}% completado.";
+                        });
+                    };
+
+                    await client.DownloadFileTaskAsync(new Uri(url), tempZipPath);
+                    lblStatus.Text = $"✅ Descarga completada. Iniciando descompresión...";
+                }
+
+                // --- 3. Descomprimir el Archivo ZIP ---
+
+                if (tempZipPath.EndsWith(".zip", StringComparison.OrdinalIgnoreCase))
+                {
+                    lblStatus.Text = "Iniciando descompresión manual (sobrescritura garantizada)...";
+
+                    // Usamos ZipFile.OpenRead y un bucle para manejar manualmente la sobrescritura.
+                    using (ZipArchive archive = ZipFile.OpenRead(tempZipPath))
+                    {
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            // Ruta completa de destino (raíz del USB + ruta interna del ZIP)
+                            string destinationFile = Path.Combine(destinationPath, entry.FullName);
+
+                            // 1. Manejo de Directorios (Entries que terminan en / o tienen longitud 0)
+                            if (string.IsNullOrEmpty(entry.Name)) // Es un directorio
+                            {
+                                // Aseguramos que la carpeta exista
+                                Directory.CreateDirectory(destinationFile);
+                            }
+                            else // 2. Manejo de Archivos
+                            {
+                                // Aseguramos que la carpeta que contendrá el archivo exista
+                                Directory.CreateDirectory(Path.GetDirectoryName(destinationFile));
+
+                                // Si el archivo ya existe en el USB, lo borramos primero
+                                if (File.Exists(destinationFile))
+                                {
+                                    File.Delete(destinationFile);
+                                }
+
+                                // Extraemos el archivo
+                                entry.ExtractToFile(destinationFile);
+                            }
+                        }
+                    }
+                    lblStatus.Text = $"✅ Descompresión completada en la raíz de {selectedDrive}";
+                }
+                else
+                {
+                    // Si no es ZIP, simplemente lo movemos a la raíz del USB
+                    string finalFilePath = Path.Combine(destinationPath, fileName);
+                    File.Move(tempZipPath, finalFilePath);
+                    lblStatus.Text = $"⚠️ Advertencia: El archivo no es ZIP. Movido a la raíz de {selectedDrive} sin descomprimir.";
+                }
+
+                // --- 4. Limpieza ---
+                File.Delete(tempZipPath);
+                lblStatus.Text = $"✅ Tarea finalizada. Archivo temporal eliminado. USB listo.";
+            }
+            catch (WebException wex)
+            {
+                lblStatus.Text = $"❌ Error de red/HTTP: {wex.Message}";
+            }
+            catch (InvalidDataException)
+            {
+                lblStatus.Text = "❌ Error: El archivo descargado no es un archivo ZIP válido o está corrupto.";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"❌ Error crítico durante la operación: {ex.Message}";
+            }
+            finally
+            {
+                // Limpieza final, incluso si hay fallos
+                if (File.Exists(tempZipPath))
+                {
+                    try { File.Delete(tempZipPath); } catch { /* Intenta limpiar */ }
+                }
+            }
+        }
+
+        // Opcional: Función para el botón Refrescar
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadUSBDrives();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.youtube.com/channel/UClh9mrZULUGRQf-2oqPnPaw");
+        }
+    }
+}
